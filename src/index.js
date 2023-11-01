b76948e5e00d8afa89c240705de2fe64c0837df0
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-10-31 23:54:53 -0400

    built basic structure for header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import './styles.css';
-
+const content = document.getElementById('content');
 function buildHome(){
-   const content = document.getElementById('content');
+
    content.textContent = "Hi, I'm Homer Simpson";
-   content.classList.add('hello');
    const title = document.createElement('div');
    const header = document.createElement('div');
+   header.textContent = "HI Im Maggie";
+   buildHeader(header);
    const body  = document.createElement('div');
    const footer = document.createElement('div');
 
 }
+
+function buildHeader(header){
+
+   const home = document.createElement('div');
+   const menu = document.createElement('div');
+   const contact = document.createElement('div');
+
+   home.textContent = "Ha";
+   menu.textContent = "Ha";
+   contact.textContent = "Ha";
+
+   const donut_home = document.createElement('img');
+   const donut_menu = document.createElement('img');
+   const donut_contact = document.createElement('img');
+
+   home.appendChild(donut_home);
+   menu.appendChild(donut_menu);
+   contact.appendChild(donut_contact);
+
+   header.appendChild(home);
+   header.appendChild(menu);
+   header.appendChild(contact);
+
+   content.appendChild(header);
+
+}
+
 
 buildHome();
