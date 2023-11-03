20b38b6bea8fc483aa24f5ed69cafa134b278090
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-11-03 00:19:05 -0400

    added grid display for menu items page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './styles.css';
 const content = document.getElementById('content');
 function buildHome(){
 
-
    const title = document.createElement('div');
    title.textContent = "The Simpson's Donut Shop";
    content.appendChild(title);
@@ -10,7 +9,8 @@ function buildHome(){
    nav.textContent = "HI Im Maggie";
    buildNavigation(nav);
    const body  = document.createElement('div');
-   buildBody(body);
+   buildMenuBody(body);
+   //buildBody(body);
 
    const footer = document.createElement('div');
    buildFooter(footer);
@@ -87,6 +87,47 @@ function buildFooter(footer){
 
    content.appendChild(footer);
 
+}
+
+
+function buildMenuBody(body){
+
+   const gridContainer = document.createElement('div');
+   gridContainer.setAttribute("id",'grid-container');
+
+   for (let i = 1; i <= 8; i++ ){
+    const cell = document.createElement('div');
+    cell.setAttribute("id",`cell-${i}`);
+    cell.textContent = "Yeahh";
+    cell.classList.add('grid-cell');
+
+    const img = document.createElement('img');
+    img.classList.add('menu-img');
+    cell.appendChild(img);
+
+    const itemContainer = document.createElement('div');
+    itemContainer.setAttribute("id",`item-${i}`);
+    const itemName = document.createElement('div');
+    const priceName = document.createElement('div');
+    itemName.classList.add('item');
+    priceName.classList.add('price');
+    itemContainer.classList.add('item-container');
+    itemContainer.appendChild(itemName);
+    itemContainer.appendChild(priceName);
+    cell.appendChild(itemContainer);
+    
+    const description = document.createElement('div');
+    description.setAttribute("id",`text-${i}`)
+    description.textContent = `cell-${i}`
+    description.classList.add('item-description');
+    cell.appendChild(description);
+
+    gridContainer.appendChild(cell);
+
+   }
+  body.appendChild(gridContainer);
+  content.appendChild(body);
+   
 }
 
 buildHome();
